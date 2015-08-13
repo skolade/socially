b6d1f9337e08954b5b0d57eb4d7c10a1c60351b4
segun Kolade <jkolade@novashock.net>
2015-08-13 12:22:09 -0500

    remove push folder, and update angularMaterial theme name

--- client/push/push.controller.ng.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-angular.module('sociallyApp')
-.controller('PushCtrl', function($scope) {
-  $scope.viewName = 'Push';
-});
--- client/styles/theme.js
+++ client/styles/theme.js
@@ -27,7 +27,7 @@ angular.module('app')
           'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
           'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
         });
-    $mdThemingProvider.theme('MPIT')
+    $mdThemingProvider.theme('socially')
       .primaryPalette('teal', {
         'default': '500', // by default use shade 400 from the deep-orange palette for primary intentions
         'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
